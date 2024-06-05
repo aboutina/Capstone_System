aec0f19069afdfce870aa04edc8994833e31a073
Jol0o <jloyd9836@gmail.com>
2024-06-05 22:27:49 +0800

    chore: Update PDFTemplate layout and content

--- src/components/pdf_template/PDFTemplate.js
+++ src/components/pdf_template/PDFTemplate.js
@@ -12,14 +12,15 @@ export const PDFTemplate = ({ name }) => {
   return (
     <>
       <Tailwind>
-        <PageTop>
-          <div className="flex items-center justify-between max-h-[100px] mt-10">
+          <div className="flex items-center mt-5 justify-evenly">
             <div className="flex flex-col items-center gap-5">
               <h6 className="text-[10px]">Civil Service Form No. 6 Revised 2020 </h6>
-              <img className="w-10 h-10 block" src="https://cvsu-rosario.edu.ph/wp-content/uploads/2019/11/logo.png" alt="logo" />
+              <img className="w-10 h-10 mt-5" src="https://cvsu-rosario.edu.ph/wp-content/uploads/2019/11/logo.png" alt="logo" />
             </div>
-            <div className="flex items-center justify-center flex-1 h-full">
-              <h1 className="text-lg font-bold text-center">Application For Leave</h1>
+            <div className="flex flex-col items-center justify-center flex-1 h-full">
+              <h6 className="text-[10px]">(Agency name)</h6>
+              <h6 className="text-[10px]">(Agency address)</h6>
+              <h1 className="mt-5 text-lg font-bold text-center">Application For Leave</h1>
             </div>
             <div className="flex flex-col gap-5">
               <h3 className="capitalize">
@@ -27,8 +28,7 @@ export const PDFTemplate = ({ name }) => {
               </h3>
               <p className="text-[10px]">{formatDate(date)}</p>
             </div>
-          </div>
-        </PageTop>
+          </div>  
       </Tailwind>
     </>
   );
